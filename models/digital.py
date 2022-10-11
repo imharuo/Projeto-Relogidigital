1c8d196409ade978b7a8ac20fa686ef5551369fc
Christian Haruo <104368550+imharuo@users.noreply.github.com>
2022-10-11 12:03:24 -0300

    Update digital.py

--- models/digital.py
+++ models/digital.py
@@ -10,27 +10,56 @@ front_color: str = '#ede593'
 # window configuration
 window = Tk()
 window.title('Relógio Digital')
-window.geometry('440x180')
+window.geometry('460x180')
 window.configure(bg=back_color)
 window.resizable(width=False, height=False)
+pyglet.font.add_file('alarmclock.ttf')
 pyglet.font.add_file('digital-7.ttf')
 
 # time/hour
-
+dias = {'Monday': 'Segunda', 'Tuesday': 'Terca', 'Wednesday': 'Quarta', 'thursday': 'Quinta',
+        'Friday': 'Sexta', 'Saturday': 'Sabado', 'Sunday': 'Domingo'}
 
 def relogio():
     tempo: datetime = datetime.now()
 
     hora: str = tempo.strftime('%H:%M:%S')
-    dia_semana: str = tempo.strftime('%A')
+    dia_semana: str = tempo.strftime('%A') # a retorna o dia da semana por extenso
     dia: tempo = tempo.day
-    mes: tempo = tempo.strftime('%b')
-    ano: tempo = tempo.strftime('%Y')
-    if dia_semana == 'Monday':
-        dia_semana = 'Segunda'
+    mes: str = tempo.strftime('%b')  # b abrevia nome do mes
+    ano: str = tempo.strftime('%Y')
 
-    if mes == 'Oct':
+    
+    if dia_semana == 'Monday':
+       dia_semana = dias['Monday']
+    elif dia_semana == 'Tuesday':
+        dia_semana = dias['Tuesday']
+    elif dia_semana == 'Wednesday':
+        dia_semana = dias['Wednesday']
+    elif dia_semana == 'Thursday':
+        dia_semana = dias['Thursday']
+    elif dia_semana == 'Friday':
+        dia_semana = dias['Friday']
+    elif dia_semana == 'Saturday':
+        dia_semana = dias['Saturday']
+    elif dia_semana == 'Sunday':
+        dia_semana = dias['Sunday']    
+
+    if mes == 'Feb':
+        mes = 'Fev'
+    elif mes =='Apr':
+        mes = 'Abr'
+    elif mes =='May':
+        mes = 'Maio'
+    elif mes =='Aug':
+        mes = 'Ago'
+    elif mes =='Sep':
+        mes = 'Set'
+    elif mes =='Oct':
         mes = 'Out'
+    elif mes =='Dec':
+        mes = 'Dez'
+    
 
     l1.config(text=hora)
     l1.after(200, relogio)
@@ -38,13 +67,14 @@ def relogio():
 
 
 # labels time
-l1 = Label(window, text='hora atual', font=("digital-7 80"), bg=back_color, fg=front_color)
-l1.grid(row=0, column=0)
+l1 = Label(window, text='hora atual', font=('"alarm clock" 80'), bg=back_color, fg=front_color)
+l1.grid(row=0, column=0, sticky=W)
 
-l2 = Label(window, text='Segunda 10/10/2022', font=("digital-7 20"), bg=back_color, fg=front_color)
-l2.grid(row=1, column=0, padx=5, sticky=NW)
+l2 = Label(window, text='Segunda 10/10/2022', font=('digital-7 20'), bg=back_color, fg=front_color)
+l2.grid(row=1, column=0, sticky=S)
 
 
 
 relogio()
 window.mainloop()
+
